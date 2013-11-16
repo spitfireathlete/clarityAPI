3aff021cc77f95edd6567c3a4946762b6ecf0613
nidhik <nidhikulkarni82@gmail.com>
2013-11-15 21:43:09 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClarityAPI::Application.config.secret_key_base = '8e4cb8250b2fb43d3cb4ed634fc6866c741b4af263352b9454fb6c372c9ae7ac26d944018a9a912f331b0d6b5880ea653a76bc7fbb241eebdaecae046c526aeb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ClarityAPI::Application.config.session_store :cookie_store, key: '_clarityAPI_session'
