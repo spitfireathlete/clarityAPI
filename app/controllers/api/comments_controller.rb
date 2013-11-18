db0a5709199d5cfd9390e91d860bc419efb80b80
nidhik <nidhikulkarni82@gmail.com>
2013-11-18 14:04:53 -0800

    same for comments. nested resource off of ideas: projects/project_id/ideas/idea_id/comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,11 +1,11 @@
 module Api
   class CommentsController < ApiController
-
+    
+    before_action :set_idea
     before_action :set_comment, only: [:show, :edit, :update, :destroy]
     
-    # get all comments where you are the author
     def index
-      @comments = Comment.where(:user_id => current_user.id)
+      @comments = Comment.where(:idea_id => @idea.id)
       respond_with @comments
     end
     
@@ -30,12 +30,18 @@ module Api
     
     
     private
+    
+    def set_idea
+      @idea = Idea.find(params[:idea_id])
+    end
+    
     def set_comment
       @comment = Comment.find(params[:id])
     end
       
     def comment_params
-      params.require(:comment).permit(:text, :idea_id)      
+      params.require(:comment)
+      params.permit(:text, :idea_id)     
     end
      
   end
